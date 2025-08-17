2f2bc11bc91332a755474cc20a59a357a6d9b309
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2025-08-16 17:55:28 -0700

    ternaory operator if else if else and or and and operator use case
    0 false and it is consider to use switch statement or? ifelse conditions statements if dealing with multiple situations, conditions

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-fetch("api")
-.then((res)=>res.json())
-.then((res)=>console.log(res))
-.catch((err)=>console.log(err));
+// fetch("api")
+// .then((res)=>res.json())
+// .then((res)=>console.log(res))
+// .catch((err)=>console.log(err));
 
-let data = new Promise(
-    fetch("api")
-    .then((res)=>res.json)
-    .then((res)=>console.log(res))
-    .catch((err)=>console.log(err))
-);
+// let data = new Promise(
+//     fetch("api")
+//     .then((res)=>res.json)
+//     .then((res)=>console.log(res))
+//     .catch((err)=>console.log(err))
+// );
 
-let getdata = data
+// let getdata = data
+
+ternary operator example
+let marks = 5;
+let type = marks >= 20 ? "eligible" : marks >=10 ? "retest" : "not eligible";
+console.log(type)
+
+let markn = 0;
+let resultn = marks && "notavailable";
+console.log(resultn);
+
+let marko = 0;
+let resulto = marks || "notavailable";
+console.log(resultn);
+
 
